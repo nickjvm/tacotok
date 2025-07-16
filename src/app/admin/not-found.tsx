9e620aae54a0236411882e45323101d349ebc2cb
Nick VanMeter <nickjvm@gmail.com>
2025-07-16 12:49:29 -0600

    update not found templates

--- src/app/admin/not-found.tsx
+++ src/app/admin/not-found.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function NotFound() {
-  return <div>404 - Page Not Found</div>;
+  return (
+    <div className="flex flex-col items-center justify-center grow">
+      <h2 className="text-2xl font-bold">That page doesn&apos;t exist.</h2>
+      <Link
+        href="/admin/all"
+        className="mt-4 px-8 py-2 border border-black-200 hover:bg-lime-300 transition-colors"
+      >
+        Back to list
+      </Link>
+    </div>
+  );
 }
--- src/app/archive/[id]/not-found.tsx
+++ src/app/archive/[id]/not-found.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center grow">
-      <h2 className="text-2xl font-bold">That page doesn&apos;t exist.</h2>
+      <h2 className="text-2xl font-bold">That recipe doesn&apos;t exist.</h2>
       <Link
-        href="/"
+        href="/archive"
         className="mt-4 px-8 py-2 border border-black-200 hover:bg-lime-300 transition-colors"
       >
-        Go Home
+        Back to archive
       </Link>
     </div>
   );
